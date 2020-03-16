900ebf9af1eb7221c9ee29a9b069bb92acf90637
Fisher16 <mkpubliczny@gmail.com>
2020-03-16 01:32:18 +0100

    Sys1 measurement

--- Zad1/entropy_test.sh
+++ Zad1/entropy_test.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 size=$1
-start_S=$(cat /proc/sys/kernel/random/entropy_avail)
+
 echo "Reading $size"
 sleep_val=$2
+echo $(cat /proc/sys/kernel/random/entropy_avail)
 for i in `seq 10`
 do
-    head -c $size /dev/random > /dev/null
+    start_S=$(cat /proc/sys/kernel/random/entropy_avail)
+    if [ $size -gt 0 ];then
+        head -c $size /dev/random > /dev/null
+    fi
     sleep $sleep_val
+    stop_S=$(cat /proc/sys/kernel/random/entropy_avail)
+    delta=$(($stop_S-$start_S))
+    echo "Delta entropy $(echo $delta/$sleep_val | bc -l)"
 done
-
-stop_S=$(cat /proc/sys/kernel/random/entropy_avail)
-delta=$(($stop_S-$start_S))
-echo "Delta entropy $(echo $delta/(10*$sleep_val) | bc)"
+echo $(cat /proc/sys/kernel/random/entropy_avail)
